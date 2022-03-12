7dd815da6e165f1713b4fe46b590ada60bfb2ee5
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2022-03-12 14:14:57 +0800

    Add stimulusjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controller"
 
 Rails.start()
 Turbolinks.start()
