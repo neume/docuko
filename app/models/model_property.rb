e6a87aca886669321c24b7a85a11f640b537c170
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2022-03-24 00:57:56 +0800

    Fix template and model property modal issues

--- app/models/model_property.rb
+++ app/models/model_property.rb
@@ -3,7 +3,7 @@ class ModelProperty < ApplicationRecord
   has_many :instance_properties, dependent: :nullify
 
   validates :name, presence: true
-  validates :code, uniqueness: { scoped: :data_model }, presence: true,
+  validates :code, uniqueness: { scope: :data_model_id }, presence: true,
                    format: {
                      with: /\A[a-zA-Z0-9_]+\z/,
                      message: 'only allows a-z, A-Z and 0-9 and underscore'
--- app/models/template.rb
+++ app/models/template.rb
@@ -2,4 +2,6 @@ class Template < ApplicationRecord
   belongs_to :data_model
 
   has_one_attached :file
+
+  validates :name, presence: true
 end
